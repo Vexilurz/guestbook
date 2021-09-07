87b92af1d7ee9a45e6908e9ce7c840ceafd5c01d
Sergei Potekhin <vexilurz@gmail.com>
2021-09-07 16:49:58 +0300

    Using twig to show conferences
    symfony composer req twig

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -6,29 +6,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ConferenceRepository;
+use Twig\Environment;
 
 class ConferenceController extends AbstractController
 {
     /**
-     * @Route("/hello/{name}", name="homepage")
+     * @Route("/", name="homepage")
      */
-    public function index(string $name = ''): Response
+    public function index(Environment $twig, ConferenceRepository
+      $conferenceRepository): Response
     {
-//        return $this->render('conference/index.html.twig', [
-//            'controller_name' => 'ConferenceController',
-//        ]);
-      $greet = '';
-      if ($name) {
-        $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
-      }
-      return new Response(<<<EOF
-        <html>
-         <body>
-         $greet
-         <img src="/images/under-construction.gif" />
-         </body>
-        </html>
-EOF
-      );
+      return new Response($twig->render('conference/index.html.twig', [
+         'conferences' => $conferenceRepository->findAll(),
+      ]));
     }
 }
