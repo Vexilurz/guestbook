34c940fb236dc8c90b5285e4c611e697697684ca
Sergei Potekhin <vexilurz@gmail.com>
2021-09-07 16:07:05 +0300

    Add EasyAdmin
    symfony composer req admin
    php bin/console make:admin:dashboard
    php bin/console make:admin:crud
    https://symfony.com/doc/current/the-fast-track/ru/9-backend.html

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller\Admin;
+use App\Entity\Comment;
+use App\Entity\Conference;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+
+class DashboardController extends AbstractDashboardController
+{
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index(): Response
+    {
+        return $this->render('@EasyAdmin/page/content.html.twig');
+
+//        $routeBuilder = $this->get(AdminUrlGenerator::class);
+//        return $this->redirect($routeBuilder->setController(ConferenceCrudController::class)->generateUrl());
+
+//        return parent::index();
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Guestbook');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Conferences', 'fas fa-list', Conference::class);
+        yield MenuItem::linkToCrud('Comments', 'fas fa-list', Comment::class);
+    }
+}
