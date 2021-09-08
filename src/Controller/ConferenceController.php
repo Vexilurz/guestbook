17546ce84d8516b56b62861d2ea31fe127e86374
Sergei Potekhin <vexilurz@gmail.com>
2021-09-08 12:50:42 +0300

    Add paginator to comments

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -16,24 +16,29 @@ class ConferenceController extends AbstractController
     /**
      * @Route("/", name="homepage")
      */
-    public function index(Environment $twig, ConferenceRepository
-      $conferenceRepository): Response
+    public function index(Environment $twig, ConferenceRepository $conferenceRepository): Response
     {
-      return new Response($twig->render('conference/index.html.twig', [
-         'conferences' => $conferenceRepository->findAll(),
-      ]));
+        return new Response($twig->render('conference/index.html.twig', [
+            'conferences' => $conferenceRepository->findAll(),
+        ]));
     }
 
     /**
-    * @Route("/conference/{id}", name="conference")
-    */
-    public function show(Environment $twig, Conference $conference,
-                         CommentRepository $commentRepository)
+     * @Route("/conference/{id}", name="conference")
+     */
+    public function show(Request $request, Environment $twig, Conference $conference, CommentRepository $commentRepository)
     {
-      return new Response($twig->render('conference/show.html.twig', [
-        'conference' => $conference,
-        'comments' => $commentRepository->findBy(['conference' =>
-          $conference], ['createdAt' => 'DESC']),
-      ]));
+        $offset = max(0, $request->query->getInt('offset', 0));
+        $paginator = $commentRepository->getCommentPaginator($conference,
+            $offset);
+
+        return new Response($twig->render('conference/show.html.twig', [
+            'conference' => $conference,
+            'comments' => $paginator,
+            'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+            'next' => min(count($paginator), $offset +
+                CommentRepository::PAGINATOR_PER_PAGE),
+
+        ]));
     }
 }
