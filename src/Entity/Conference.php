f63c9217a82753298623c95cb167f776e1be6a85
Sergei Potekhin <vexilurz@gmail.com>
2021-09-08 14:15:53 +0300

    Add slugger
    symfony composer req string

--- src/Entity/Conference.php
+++ src/Entity/Conference.php
@@ -6,6 +6,7 @@ use App\Repository\ConferenceRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 /**
  * @ORM\Entity(repositoryClass=ConferenceRepository::class)
@@ -51,12 +52,19 @@ class Conference
 
     public function __toString(): string
     {
-      return $this->city.' '.$this->year;
+        return $this->city . ' ' . $this->year;
     }
 
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function computeSlug(SluggerInterface $slugger)
+    {
+        if (!$this->slug || '-' === $this->slug) {
+            $this->slug = (string)$slugger->slug((string)$this)->lower();
+        }
     }
 
     public function getCity(): ?string
